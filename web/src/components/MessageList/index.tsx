92350e46600eab96540c3f9b758d46672442d9ca
Yuri Oliveira <yurioliveirasilva1999@gmail.com>
2021-10-23 21:09:29 -0300

    Feat.: Add MessageList Component

--- web/src/components/MessageList/index.tsx
+++ web/src/components/MessageList/index.tsx
@@ -1,5 +1,43 @@
+import logoImg from '../../assets/logo.svg'
+
+import styles from './styles.module.scss'
+
 export function MessageList(){
     return(
-        <h1>MessageList</h1>
+        <div className={ styles.messageListWrapper }>
+            <img src={ logoImg } alt="DoWhile 2021" />
+
+            <ul className={ styles.messageList }>
+                <li className={ styles.message }>
+                    <p className={ styles.messageContent }>Lorem ipsum dolor sit amet consectetur, adipisicing elit. </p>
+                    <div className={ styles.messageUser }>
+                        <div className={ styles.userImage }>
+                            <img src="https://github.com/yuri-stack.png" alt="Yuri Oliveira" />
+                        </div>
+                        <span>Yuri Oliveira</span>
+                    </div>
+                </li>
+
+                <li className={ styles.message }>
+                    <p className={ styles.messageContent }>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum aut veniam odio voluptatibus alias quisquam? Impedit odio nostrum non, omnis soluta dicta sit libero sed nemo cumque dolorem optio aut?</p>
+                    <div className={ styles.messageUser }>
+                        <div className={ styles.userImage }>
+                            <img src="https://github.com/yuri-stack.png" alt="Yuri Oliveira" />
+                        </div>
+                        <span>Yuri Oliveira</span>
+                    </div>
+                </li>
+
+                <li className={ styles.message }>
+                    <p className={ styles.messageContent }>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum aut veniam odio voluptatibus alias quisquam? Impedit odio nostrum non, omnis soluta dicta sit libero sed nemo cumque dolorem optio aut?</p>
+                    <div className={ styles.messageUser }>
+                        <div className={ styles.userImage }>
+                            <img src="https://github.com/yuri-stack.png" alt="Yuri Oliveira" />
+                        </div>
+                        <span>Yuri Oliveira</span>
+                    </div>
+                </li>
+            </ul>
+        </div>
     )
 }
