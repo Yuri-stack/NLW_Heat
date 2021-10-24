d65c300aaaae03a1850882cafdf443878bfbbb66
Yuri Oliveira <yurioliveirasilva1999@gmail.com>
2021-10-24 16:28:48 -0300

    Feat.: Backend connected messages shown in the app

--- web/src/components/MessageList/index.tsx
+++ web/src/components/MessageList/index.tsx
@@ -1,42 +1,45 @@
-import logoImg from '../../assets/logo.svg'
+import { useEffect, useState } from 'react'
+import { api } from '../../services/api'
 
+import logoImg from '../../assets/logo.svg'
 import styles from './styles.module.scss'
 
+interface Message{
+    id: string
+    text: string
+    user: {
+        name: string
+        avatar_url: string
+    }
+}
+
 export function MessageList(){
+    const [messages, setMessages] = useState<Message[]>([])
+
+    useEffect(() => {
+        api.get<Message[]>('messages/lastThree').then(response => {
+            setMessages(response.data)
+        })
+    }, [])
+
     return(
         <div className={ styles.messageListWrapper }>
             <img src={ logoImg } alt="DoWhile 2021" />
 
             <ul className={ styles.messageList }>
-                <li className={ styles.message }>
-                    <p className={ styles.messageContent }>Lorem ipsum dolor sit amet consectetur, adipisicing elit. </p>
-                    <div className={ styles.messageUser }>
-                        <div className={ styles.userImage }>
-                            <img src="https://github.com/yuri-stack.png" alt="Yuri Oliveira" />
-                        </div>
-                        <span>Yuri Oliveira</span>
-                    </div>
-                </li>
-
-                <li className={ styles.message }>
-                    <p className={ styles.messageContent }>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum aut veniam odio voluptatibus alias quisquam? Impedit odio nostrum non, omnis soluta dicta sit libero sed nemo cumque dolorem optio aut?</p>
-                    <div className={ styles.messageUser }>
-                        <div className={ styles.userImage }>
-                            <img src="https://github.com/yuri-stack.png" alt="Yuri Oliveira" />
-                        </div>
-                        <span>Yuri Oliveira</span>
-                    </div>
-                </li>
-
-                <li className={ styles.message }>
-                    <p className={ styles.messageContent }>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum aut veniam odio voluptatibus alias quisquam? Impedit odio nostrum non, omnis soluta dicta sit libero sed nemo cumque dolorem optio aut?</p>
-                    <div className={ styles.messageUser }>
-                        <div className={ styles.userImage }>
-                            <img src="https://github.com/yuri-stack.png" alt="Yuri Oliveira" />
-                        </div>
-                        <span>Yuri Oliveira</span>
-                    </div>
-                </li>
+                { messages.map(message => {
+                    return(
+                        <li key={ message.id } className={ styles.message }>
+                            <p className={ styles.messageContent }>{ message.text }</p>
+                            <div className={ styles.messageUser }>
+                                <div className={ styles.userImage }>
+                                    <img src={ message.user.avatar_url } alt={ message.user.name } />
+                                </div>
+                                <span>{ message.user.name }</span>
+                            </div>
+                        </li>
+                    )
+                })}
             </ul>
         </div>
     )
